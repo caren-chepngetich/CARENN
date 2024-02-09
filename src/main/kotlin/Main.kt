ca154884bef1d69d68e4a99017c019ce9b3ffd1a
caren chepngetich <chepngetichkosgeicarenn>
2024-02-09 07:17:16 +0300

    add files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+   var fullname : String = "Caren Chepngetich"
+   var age : Byte = 23
+   var phonenumber : String = "254795966265"
+   var kg : Float = 47F
+   var kenyan = true
+
+   println(fullname)
+   println(age)
+   println(phonenumber)
+   println(kg)
+   println(kenyan)
+
+}
